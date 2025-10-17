7dd1f439ece224ee41785fea0856455c8ed91688
Krista Lorena Rodriguez Martinez <krista23rm@gmail.com>
2025-10-16 20:02:30 -0600

    Que loooooco esta esto de Laravel

---
+++ rodriguezmartinez_20251016/app/Http/Controllers/ZonaController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZonaController extends Controller
+{
+    //
+
+    public function obtenerZonas(){
+
+        $Zona = new Zona();
+        $valores = $Zona::all();
+        $respuesta = [
+            "sucess" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores) 
+        ];
+
+        return response()->json($respuesta,200);
+
+    }
+
+    public function obtenerZona($idzona){
+
+        $Zona = new Zona();
+        $valores = $Zona->where('id_departamento',$idzona)->get();
+        $respuesta = [
+            "sucess" => true,
+            "msg" => "Valores devueltos por el EndPoint",
+            "data" => $valores,
+            "error" => "",
+            "total" => sizeof($valores) 
+        ];
+
+        return response()->json($respuesta,200);
+
+    }
+}
